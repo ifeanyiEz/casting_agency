801084e661b925c39a0733e2db77c742eb92274b
Ezugworie Ifeanyi Ofor <ofotronics.2010@gmail.com>
2022-06-23 13:09:25 +0100

    First commit

---
+++ test_app.py
@@ -0,0 +1,21 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+from settings import *
+from models import *
+from app import app
+
+class CastingTestCast(unittest.TestCase):
+
+    def setup(self):
+        '''Define test variable and initialize the test app'''
+        self.app = app()
+        self.client = self.app.test_client
+        self.database_name = TEST_DB_NAME
+        self.database_path = "postgresql://{}:{}@{}/{}".format(DB_USER, DB_PASSWORD, "localhost:5432", self.database_name)
+        setup_db(self.app, self.database_path)
+
+
+        with self.app.app_context():
+            
