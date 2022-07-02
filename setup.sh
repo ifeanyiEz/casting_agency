774be47c46b2916cb84d0ff4ef864c4bb2f843a9
Ezugworie Ifeanyi Ofor <ofotronics.2010@gmail.com>
2022-07-02 06:59:57 +0100

    Update the setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+export SECRET="EzuFSNDCastingAgencyCapstoneProject"
+export AUTH0_DOMAIN="ezufsndcap.us.auth0.com"
+export API_AUDIENCE="http://localhost:5000"
+export ALGORITHMS=['RS256']
+export FLASK_APP=app.py
+export FLASK_ENV=development
 export DATABASE_URL="postgresql://ezugworie:E2u8w0r1e@localhost:5432/casting"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
